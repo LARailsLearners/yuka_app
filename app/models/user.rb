75a3ca5615d6d24c6549811d8fa408f65b0d10be
Yuka Y <anzu28@gmail.com>
2015-07-25 10:59:51 -0700

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-#         :confirmable, :lockable, :timeoutable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable #:lockable, :timeoutable
+  
+  def confirm!
+    welcome_email
+  end
+
+  private
+    def welcome_email
+      UserMailer.welcome_email(self).deliver
+    end
 end
