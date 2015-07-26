7f1f0836b20e3c5d2949236abcdf18c0ddab17a2
Yuka Y <anzu28@gmail.com>
2015-07-26 15:53:32 -0700

    add devise files, navigation file and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710045923) do
+ActiveRecord::Schema.define(version: 20150726224931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150710045923) do
     t.float    "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
